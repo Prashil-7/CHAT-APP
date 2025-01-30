1043b6c87b045ecead769f1dc327a0d7fcc50e11
Prashil-7 <prashillonare1@gmail.com>
2025-01-30 20:56:11 +0530

    setup completed

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import authRoutes from './routes/auth-route.js';
+
+
+
+
+const app =express(); 
+const port =  3000;
+
+app.use('/api/auth', authRoutes);
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    
+});
