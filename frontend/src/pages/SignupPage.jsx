e0b5ad499ce5f9f226a6d896304bc9a0d829ef95
prashil-7 <prashillonare1@gmail.com>
2025-02-06 17:05:28 +0530

    the signup froonted is is completed

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { useAuthStore } from '../store/useAuthStore';
-import {MessageSquare , User} from 'lucide-react'
+import AuthImagePattern from '../Components/AuthImagePattern'
+import {Eye, EyeOff, Loader2, Lock, Mail, MessageSquare , User} from 'lucide-react'
+import { Link } from 'react-router-dom';
 
 const  SignupPage = () => {
-  const [showPassword ,setPassword] = useState(false);
+  const [showPassword ,setShowPassword] = useState(false);
 
   const [fromData ,setFromData] = useState({
     fullName:"",
@@ -11,7 +13,7 @@ const  SignupPage = () => {
     Password:"",
   });
 
-  const {signup , isSigningUp} = useAuthStore();
+  const {issignup , isSigningUp} = useAuthStore();
   
   const validateForm =()=>{
     
@@ -24,7 +26,7 @@ const  SignupPage = () => {
 
     <div className=' min-h-screen grid lg:grid-cols-2'>
       
-      {/* {left side} */}
+      {/* {left side that creation Account } */}
       <div className='flex flex-col justify-center items-center p-6 sm:p-12'>
         {/* {logo} */}
         <div className='w-full max-w-md space-y-8'>
@@ -53,9 +55,79 @@ const  SignupPage = () => {
                   onChange={(e)=> setFromData({...FormData ,fullName: e.target.value})} />
                 </div>
               </div>
+              {/* {email tag colm} */}
+
+              <div className='from-control'>
+                <label  className=' label'>
+                  <span className='label-text font-medium'>E-mail</span>
+                </label>
+                <div className='relative'>
+                  <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                    <Mail className='size-5 text-base-content/40'/>
+                  </div>
+                  <input type="email"
+                  className={`input input-border w-full pl-10`}
+                  placeholder='you@exsample.com'
+                  value={FormData.email}
+                  onChange={(e)=> setFromData({...FormData ,email: e.target.value})} />
+                </div>
+              </div>
+                {/* {password field with hidden password FEATURE} */}
+                <div className='from-control'>
+                <label  className=' label'>
+                  <span className='label-text font-medium'>Password</span>
+                </label>
+                <div className='relative'>
+                  <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                    <Lock className='size-5 text-base-content/40'/>
+                  </div>
+                  <input type= {showPassword ?"text": "password"}
+                  className={`input input-border w-full pl-10`}
+                  placeholder='********'
+                  value={FormData.Password}
+                  onChange={(e)=> setFromData({...FormData ,Password: e.target.value})} />
+
+                    {/* {submit btn} */}
+                  <button type="button"
+                  className='absolute inset-y-0 right-0 pr-3 flex items-center'
+                  onClick={()=>setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <EyeOff className=' size-5 text-base-content/40'/>
+                        ):(
+                        <Eye className=' size-5 text-base-content/40'/>
+                        )}
+                  </button>
+                </div>
+              </div>
+
+                      {/* { the btn of creatrin acccount} */}
+                      <button type='submit' className='btn btn-primary w-full' disabled={isSigningUp}>
+                        {isSigningUp ? (
+                          <>
+                          <Loader2 className='size-5 animate-bounce'/>
+                          Loading bhai.....
+                          </>
+                        ):(
+                          "Create Account bhai"
+                        )}
+                      </button>
             </form>
+            <div className='text-center'>
+              <p className='text-base-content/60'>
+              Already  have an account ? {" "}
+              <Link to="/login" className='link link-primary' >
+              Sign In </Link> </p>
+            </div>
         </div>
       </div>
+              
+              {/* { rigth side community side} */}
+
+          <AuthImagePattern 
+          title=" Join Our Community"
+          subtitle =" connect with friends , share moments, and stay in touch with your friends"/>
+
     </div>
 
 
